f6450bea89f66d827f5b013a5806e62ac499b814
q3cc <1292673997@qq.com>
2024-10-05 20:05:13 +0800

    尝试修bug

--- 建筑备份.lua
+++ 建筑备份.lua
@@ -10,7 +10,7 @@ local version = coromega.config.Version
 if version == "0.0.1" then -- 只有当配置文件版本较旧的时候才升级
     coromega.config["备份区域的长宽"] = "62"
     coromega.config["备份区域的高度"] = "62"
-    coromega.config["改了上面的配置记得修改源代码内的提示"] = "修改此项无效"
+    coromega.config["改了上面的配置记得修改源代码内的提示"] = "修改此项无效" --51行处
     coromega.config.Version = "0.0.2"
     coromega:update_config(coromega.config)
 end
@@ -18,7 +18,6 @@ end
 local testfor = coromega.config["改了上面的配置记得修改源代码内的提示"]
 local height = coromega.config["备份区域的高度"]
 local length = coromega.config["备份区域的长宽"]
-local back_db = coromega:key_value_db("玩家备份信息") -- 定义建筑备份数据的文件
 local player_backup_log_db = coromega:key_value_db("玩家备份日志") -- 新建一个数据库用于记录备份日志
 
 if testfor ~= "修改此项无效" then -- 帮傻子修改回去
@@ -45,7 +44,7 @@ math.randomseed(os.time()) -- 初始化随机数种子
 local back_name = generate_random_string(4) -- 生成长度为 4 的随机字符串
 
 -- 游戏内备份菜单
-coromega:when_called_by_terminal_menu({
+coromega:when_called_by_game_menu({
     triggers = { "备份", "back" },
     argument_hint = "[建筑名称]",
     usage = "自助备份大小为 62*62*62 的区域", --记得改此项
@@ -54,14 +53,6 @@ coromega:when_called_by_terminal_menu({
     local caller_name = chat.name
     local caller = coromega:get_player_by_name(caller_name)
     local set_name = input[1]
-    while not set_name or set_name == "" do
-        set_name = caller:ask("请输入建筑名称：")
-        if not set_name or set_name == "" then
-            caller:say("建筑名称不能为空")
-        end
-    end
-
-
     -- 冷却
     local current_timestamp = os.time()
     local log_entry = player_backup_log_db:get(chat.name)
@@ -76,6 +67,15 @@ coromega:when_called_by_terminal_menu({
             return -- 不执行后面的代码
         end
     end
+
+    while not set_name or set_name == "" do
+        set_name = caller:ask("请输入建筑名称：")
+        if not set_name or set_name == "" then
+            caller:say("建筑名称不能为空")
+        end
+    end
+    
+
 
     local player = coromega:get_player(chat.name) -- 获取玩家名称
     local pos = player:get_pos().position -- 获取玩家坐标
@@ -94,13 +94,12 @@ coromega:when_called_by_terminal_menu({
     local z2 = z + length
 
     -- 构建命令字符串
-    local command = string.format("/execute as %s at @s run structure save %s %s %s %s %s %s disk", 
+    local command = string.format("execute as %s at @s run structure save %s %s %s %s %s %s disk", 
         player:name(), back_name, x1, y1, z1, x2, y2)
 
+
     -- 命令执行部分
-    coromega:send_player_cmd(command) -- get_result 为 false 或者空时没有返回值
-    local result = coromega:send_player_cmd(command, true)
-    coromega:print(json.encode(result)) -- result 的结果是一个很复杂的结构
+    coromega:send_wo_cmd(command) -- get_result 为 false 或者空时没有返回值
 
     -- 加上 600 秒
     local new_timestamp = current_timestamp + 600
